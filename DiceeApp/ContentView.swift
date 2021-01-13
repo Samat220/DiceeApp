acdb6267f88cda790905e8543ec028aa60f1d203
ramazan <samatramazan@gmail.com>
2021-01-13 12:56:59 -0500

    Initial Commit

---
+++ DiceeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceeApp
+//
+//  Created by Ramazan Samat on 13/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceeApp/DiceeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeAppApp.swift
+//  DiceeApp
+//
+//  Created by Ramazan Samat on 13/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
